f15897792fe8048ec25e32d53e036095554280a3
Benjamin Cutler <ua@uranium-anchor.com>
2014-07-08 04:34:10 -0600

    Fix a javascript include on the multi-select widget

--- ajax_select/fields.py
+++ ajax_select/fields.py
@@ -146,7 +146,7 @@ class AutoCompleteSelectMultipleWidget(forms.widgets.SelectMultiple):
     add_link = None
 
     class Media:
-        js = ['admin/js/jquery.min.js','js/ajax_select.js']
+        js = [settings.STATIC_URL + 'admin/js/jquery.min.js','js/ajax_select.js']
         css = {'ajax': ['css/ajax_select.css']}
 
     def __init__(self,
